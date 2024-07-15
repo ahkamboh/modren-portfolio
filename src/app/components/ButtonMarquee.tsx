36e10a72808075ed41f04552ae0b401e15916b0f
kambohdiv <kambohdiv@gmail.com>
2024-07-15 11:24:42 +0500

    initial

---
+++ src/app/components/ButtonMarquee.tsx
@@ -0,0 +1,95 @@
+import React from 'react'
+import Marquee from 'react-fast-marquee'
+function ButtonMarquee() {
+  return (
+ <div>
+    <div className=" space-y-4">
+      <Marquee direction="left" pauseOnHover className='cursor-pointer' >
+      <div className="w-fit h-7  px-5 py-5 mx-2 bg-gradient-to-b from-[#2c2a2a] to-neutral-900 rounded-3xl border border-neutral-400 justify-center items-center gap-1 inline-flex">
+        <div className="text-white text-sm ClashDisplay-Light">Zeus Learning</div>
+      </div>
+      <div className="w-fit h-7 px-5 py-5 mx-2 bg-gradient-to-b from-[#2c2a2a] to-neutral-900 rounded-3xl border border-neutral-400 justify-center items-center gap-1 inline-flex">
+        <div className="text-white text-sm ClashDisplay-Light">LinkedIn</div>
+      </div>
+      <div className="w-fit h-7 px-5 py-5 mx-2 bg-gradient-to-b from-[#2c2a2a] to-neutral-900 rounded-3xl border border-neutral-400 justify-center items-center gap-1 inline-flex">
+        <div className="text-white text-sm ClashDisplay-Light">Facebook</div>
+      </div>
+      <div className="w-fit h-7 px-5 py-5 mx-2 bg-gradient-to-b from-[#2c2a2a] to-neutral-900 rounded-3xl border border-neutral-400 justify-center items-center gap-1 inline-flex">
+        <div className="text-white text-sm ClashDisplay-Light">Instagram</div>
+      </div>
+      <div className="w-fit h-7 px-5 py-5 mx-2 bg-gradient-to-b from-[#2c2a2a] to-neutral-900 rounded-3xl border border-neutral-400 justify-center items-center gap-1 inline-flex">
+        <div className="text-white text-sm ClashDisplay-Light">Pakweels</div>
+      </div>
+      <div className="w-fit h-7 px-5 py-5 mx-2 bg-gradient-to-b from-[#2c2a2a] to-neutral-900 rounded-3xl border border-neutral-400 justify-center items-center gap-1 inline-flex">
+        <div className="text-white text-sm ClashDisplay-Light">Google</div>
+      </div>
+      <div className="w-fit h-7 px-5 py-5 mx-2 bg-gradient-to-b from-[#2c2a2a] to-neutral-900 rounded-3xl border border-neutral-400 justify-center items-center gap-1 inline-flex">
+        <div className="text-white text-sm ClashDisplay-Light">Zeus Learning</div>
+      </div>
+      <div className="w-fit h-7 px-5 py-5 mx-2 bg-gradient-to-b from-[#2c2a2a] to-neutral-900 rounded-3xl border border-neutral-400 justify-center items-center gap-1 inline-flex">
+        <div className="text-white text-sm ClashDisplay-Light">LinkedIn</div>
+      </div>
+      <div className="w-fit h-7 px-5 py-5 mx-2 bg-gradient-to-b from-[#2c2a2a] to-neutral-900 rounded-3xl border border-neutral-400 justify-center items-center gap-1 inline-flex">
+        <div className="text-white text-sm ClashDisplay-Light">Facebook</div>
+      </div>
+      <div className="w-fit h-7 px-5 py-5 mx-2 bg-gradient-to-b from-[#2c2a2a] to-neutral-900 rounded-3xl border border-neutral-400 justify-center items-center gap-1 inline-flex">
+        <div className="text-white text-sm ClashDisplay-Light">Instagram</div>
+      </div>
+      <div className="w-fit h-7 px-5 py-5 mx-2 bg-gradient-to-b from-[#2c2a2a] to-neutral-900 rounded-3xl border border-neutral-400 justify-center items-center gap-1 inline-flex">
+        <div className="text-white text-sm ClashDisplay-Light">Pakweels</div>
+      </div>
+      <div className="w-fit h-7 px-5 py-5 mx-2 bg-gradient-to-b from-[#2c2a2a] to-neutral-900 rounded-3xl border border-neutral-400 justify-center items-center gap-1 inline-flex">
+        <div className="text-white text-sm ClashDisplay-Light">Google</div>
+      </div>
+      <div className="w-fit h-7 px-5 py-5 mx-2 bg-gradient-to-b from-[#2c2a2a] to-neutral-900 rounded-3xl border border-neutral-400 justify-center items-center gap-1 inline-flex">
+        <div className="text-white text-sm ClashDisplay-Light">Instagram</div>
+      </div>
+    </Marquee>
+
+    <Marquee direction="right" pauseOnHover className='cursor-pointer' >
+      <div className="w-fit h-7 px-5 py-5 mx-2 bg-gradient-to-b from-[#2c2a2a] to-neutral-900 rounded-3xl border border-neutral-400 justify-center items-center gap-1 inline-flex">
+        <div className="text-white text-sm ClashDisplay-Light">Zeus Learning</div>
+      </div>
+      <div className="w-fit h-7 px-5 py-5 mx-2 bg-gradient-to-b from-[#2c2a2a] to-neutral-900 rounded-3xl border border-neutral-400 justify-center items-center gap-1 inline-flex">
+        <div className="text-white text-sm ClashDisplay-Light">LinkedIn</div>
+      </div>
+      <div className="w-fit h-7 px-5 py-5 mx-2 bg-gradient-to-b from-[#2c2a2a] to-neutral-900 rounded-3xl border border-neutral-400 justify-center items-center gap-1 inline-flex">
+        <div className="text-white text-sm ClashDisplay-Light">Facebook</div>
+      </div>
+      <div className="w-fit h-7 px-5 py-5 mx-2 bg-gradient-to-b from-[#2c2a2a] to-neutral-900 rounded-3xl border border-neutral-400 justify-center items-center gap-1 inline-flex">
+        <div className="text-white text-sm ClashDisplay-Light">Instagram</div>
+      </div>
+      <div className="w-fit h-7 px-5 py-5 mx-2 bg-gradient-to-b from-[#2c2a2a] to-neutral-900 rounded-3xl border border-neutral-400 justify-center items-center gap-1 inline-flex">
+        <div className="text-white text-sm ClashDisplay-Light">Pakweels</div>
+      </div>
+      <div className="w-fit h-7 px-5 py-5 mx-2 bg-gradient-to-b from-[#2c2a2a] to-neutral-900 rounded-3xl border border-neutral-400 justify-center items-center gap-1 inline-flex">
+        <div className="text-white text-sm ClashDisplay-Light">Google</div>
+      </div>
+      <div className="w-fit h-7 px-5 py-5 mx-2 bg-gradient-to-b from-[#2c2a2a] to-neutral-900 rounded-3xl border border-neutral-400 justify-center items-center gap-1 inline-flex">
+        <div className="text-white text-sm ClashDisplay-Light">Zeus Learning</div>
+      </div>
+      <div className="w-fit h-7 px-5 py-5 mx-2 bg-gradient-to-b from-[#2c2a2a] to-neutral-900 rounded-3xl border border-neutral-400 justify-center items-center gap-1 inline-flex">
+        <div className="text-white text-sm ClashDisplay-Light">LinkedIn</div>
+      </div>
+      <div className="w-fit h-7 px-5 py-5 mx-2 bg-gradient-to-b from-[#2c2a2a] to-neutral-900 rounded-3xl border border-neutral-400 justify-center items-center gap-1 inline-flex">
+        <div className="text-white text-sm ClashDisplay-Light">Facebook</div>
+      </div>
+      <div className="w-fit h-7 px-5 py-5 mx-2 bg-gradient-to-b from-[#2c2a2a] to-neutral-900 rounded-3xl border border-neutral-400 justify-center items-center gap-1 inline-flex">
+        <div className="text-white text-sm ClashDisplay-Light">My Captain</div>
+      </div>
+      <div className="w-fit h-7 px-5 py-5 mx-2 bg-gradient-to-b from-[#2c2a2a] to-neutral-900 rounded-3xl border border-neutral-400 justify-center items-center gap-1 inline-flex">
+        <div className="text-white text-sm ClashDisplay-Light">Pakweels</div>
+      </div>
+      <div className="w-fit h-7 px-5 py-5 mx-2 bg-gradient-to-b from-[#2c2a2a] to-neutral-900 rounded-3xl border border-neutral-400 justify-center items-center gap-1 inline-flex">
+        <div className="text-white text-sm ClashDisplay-Light">Google</div>
+      </div>
+      <div className="w-fit h-7 px-5 py-5 mx-2 bg-gradient-to-b from-[#2c2a2a] to-neutral-900 rounded-3xl border border-neutral-400 justify-center items-center gap-1 inline-flex">
+        <div className="text-white text-sm ClashDisplay-Light">Instagram</div>
+      </div>
+     </Marquee>
+    </div>
+ </div>
+  )
+}
+
+export default ButtonMarquee
---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,71 @@
+"use client"
+import React, { useState } from 'react';
+import Image from 'next/image';
+
+function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <div>
+      <div className="container mx-auto">
+        <div className="list-none flex justify-between p-5">
+          <div className="">
+            <Image src="/logo.svg" width={60} height={60} alt="Nav Logo" />
+          </div>
+          <div className="sm:flex hidden gap-5 ClashDisplay-Regular">
+            <li className=' text-white hover:text-blue-500 transition-colors duration-300'><a href="">Home</a></li>
+            <li className='text-white hover:text-blue-500 transition-colors duration-300'><a href="">About</a></li>
+            <li className='text-white hover:text-blue-500 transition-colors duration-300'><a href="">Work</a></li>
+            <li className='text-white hover:text-blue-500 transition-colors duration-300'><a href="">Contact</a></li>
+          </div>
+          <div className="flex gap-2 ClashDisplay-Semibold justify-center items-center">
+            <li>
+              <span
+                className='sm:hidden block cursor-pointer text-white hover:text-blue-500 transition-colors duration-300'
+                onClick={toggleMenu}
+              >
+                Menu
+              </span>
+              <a className='sm:block hidden text-white hover:text-blue-500 transition-colors duration-300' href="">Resume</a>
+            </li>
+            <li className='sm:block hidden'>
+              <Image
+                src="/download.svg"
+                width={12}
+                height={12}
+                alt="resume icon"
+                className='sm:block hidden'
+              />
+            </li>
+          </div>
+        </div>
+
+        <div
+          onClick={toggleMenu}
+          className={`${
+            menuOpen ? 'flex' : 'hidden'
+          } sm:hidden h-screen absolute top-0 z-40 items-center justify-center list-none w-full transition-opacity duration-500 ease-in-out`}
+          style={{
+            backdropFilter: 'blur(5px)',
+            backgroundColor: 'rgba(154, 130, 130, 0.21)',
+            opacity: menuOpen ? 1 : 0
+          }}
+        >
+          <li className='text-white rotate-45 absolute top-10 right-10 hover:bg-black transition-colors duration-100 rounded-full p-1 bg-transparent '><img src="/cross.svg" alt="" /></li>
+          <div className="space-y-5 text-center text-xl ClashDisplay-Regular w-full relative">
+            <li className='text-white hover:text-blue-500 transition-colors duration-300'><a href="">Home</a></li>
+            <li className='text-white hover:text-blue-500 transition-colors duration-300'><a href="">About</a></li>
+            <li className='text-white hover:text-blue-500 transition-colors duration-300'><a href="">Work</a></li>
+            <li className='text-white hover:text-blue-500 transition-colors duration-300'><a href="">Contact</a></li>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
