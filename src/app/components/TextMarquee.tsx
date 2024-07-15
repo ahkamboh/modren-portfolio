d94c0c05bbcfe08db2c6f19f42503f3127435afb
Ali Hamza Kamboh <123060177+ahkamboh@users.noreply.github.com>
2024-07-15 12:01:35 +0500

    update nav img

--- src/app/components/TextMarquee.tsx
+++ src/app/components/TextMarquee.tsx
@@ -4,17 +4,17 @@ function TextMarquee() {
   return (
     <Marquee direction="left" pauseOnHover >
       <div className="text-white lg:text-3xl text-xl ClashDisplay-Medium ">Visual Design</div>
-      <img src="SoftStar.png" alt="star" className='px-4' />
+      <img src="/SoftStar.png" alt="star" className='px-4' />
       <div className="text-white lg:text-3xl text-xl  ClashDisplay-Medium">Rapid Prototyping</div>
-      <img src="SoftStar.png" alt="star"  className='px-4' />
+      <img src="/SoftStar.png" alt="star"  className='px-4' />
       <div className="text-white lg:text-3xl text-xl  ClashDisplay-Medium">User Testing</div>
-      <img src="SoftStar.png" alt="star"  className='px-4' />
+      <img src="/SoftStar.png" alt="star"  className='px-4' />
       <div className="text-white lg:text-3xl text-xl  ClashDisplay-Medium">Design Systems</div>
-      <img src="SoftStar.png" alt="star"  className='px-4' />
+      <img src="/SoftStar.png" alt="star"  className='px-4' />
       <div className="text-white lg:text-3xl text-xl  ClashDisplay-Medium">Graphic Design</div>
-      <img src="SoftStar.png" alt="star"  className='px-4' />
+      <img src="/SoftStar.png" alt="star"  className='px-4' />
       <div className="text-white lg:text-3xl text-xl  ClashDisplay-Medium">BBC Studios</div>
-      <img src="SoftStar.png" alt="star"  className='px-4' />
+      <img src="/SoftStar.png" alt="star"  className='px-4' />
       <div className="text-white lg:text-3xl text-xl  ClashDisplay-Medium">Blizzard</div>
     </Marquee>
   )
